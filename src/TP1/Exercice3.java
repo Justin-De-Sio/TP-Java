a0f7d477142594933812d337b62e4f71d7bfe116
Justin De Sio <justin.desio@edu.ece.fr>
2022-09-15 16:54:46 +0200

    Exo3 tp1

--- src/TP1/Exercice3.java
+++ src/TP1/Exercice3.java
@@ -4,18 +4,34 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Exercice3 {
-    public void tirage(){
+    public void tirage() {
         int min = 1;
         int max = 49;
-
-        int[] tab = new int[6];
+        int[] loto = new int[6];
 
         Random random = new Random();
-        int randomNumber = random.nextInt(min + max) + min;
-        for (int i = 0; i < tab.length; i++) {
-            System.out.println(i);
+
+        for (int i = 0; i < loto.length; i++) {
+            int randomNumber = random.nextInt(min + max) + min;
+            if (exist(loto,randomNumber)) {
+                i--;
+            } else {
+                loto[i] = randomNumber;
+            }
+        }
+        System.out.println("Les numéros gagnants sont :");
+        for (int number : loto) {
+            System.out.println(number);
         }
 
+    }
 
+    //TODO exo 3,4,6,7
+    public boolean exist(int[] list, int number) {
+        for (int element : list) {
+            if (element == number)
+                return true;
+        }
+        return false;
     }
 }
